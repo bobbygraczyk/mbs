457ad9fba7e3d66c2d8295b348098fd8f9ee0ad8
bobbygraczyk <bobbygraczyk@gmail.com>
2021-01-16 11:00:22 -0500

    Updated contact page info

--- src/views/contactPage.js
+++ src/views/contactPage.js
@@ -17,12 +17,12 @@ class ContactPage extends React.Component {
                 <div className="frame form">
                     <div id="info">
                         <Icons name="phone" />
-                        <p id="phone">(330) 321-3211</p>
+                        <p id="phone">(234) 281-4996</p>
                         <Icons name="email" />
                         <p id="email">drstacey@mbschiro.com</p>
                         <Icons name="info" />
                         <p id="hours">M - F / 9AM - 5PM</p>
-                        <p id="copy">Your can reach Dr. Stacey by phone and email, or you may fill out the contact form below to request that Dr. Stacey reach out to you.</p>
+                        <p id="copy">You can reach Dr. Stacey by call, text, or email. Or, you may fill out the contact form below to request that Dr. Stacey reach out to you.</p>
                     </div>
                     
                     <iframe title="Contact Form" style={{ height: "1000px", width: "100%", margin: "0 auto", overflow: "hidden"}} src="https://docs.google.com/forms/d/e/1FAIpQLSfz3xJsEj8VKELg-vorpZt54PgWizrI00ByXkwnT7lArAM9FA/viewform?embedded=true" width="640" height="808" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
