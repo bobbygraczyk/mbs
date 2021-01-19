3172c139854156c88056c78926ceb497ef98f9f1
twoonesixdigital <team@216digital.com>
2021-01-18 22:13:59 -0500

    Fixed visual bugs and swapped form

--- src/views/contactPage.js
+++ src/views/contactPage.js
@@ -19,13 +19,12 @@ class ContactPage extends React.Component {
                         <Icons name="phone" />
                         <p id="phone">(234) 281-4996</p>
                         <Icons name="email" />
-                        <p id="email">drstacey@mbschiro.com</p>
+                        <p id="email">drstacey@mbschiro2go.com</p>
                         <Icons name="info" />
                         <p id="hours">M - Th / 10AM - 7PM</p>
                         <p id="copy">You can reach Dr. Stacey by call, text, or email. Or, you may fill out the contact form below to request that Dr. Stacey reach out to you.</p>
                     </div>
-                    
-                    <iframe title="Contact Form" style={{ height: "1000px", width: "100%", margin: "0 auto", overflow: "hidden"}} src="https://docs.google.com/forms/d/e/1FAIpQLSfz3xJsEj8VKELg-vorpZt54PgWizrI00ByXkwnT7lArAM9FA/viewform?embedded=true" width="640" height="808" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                     <iframe id="JotFormIFrame-210055091090039" title="Contact Us" onload="window.parent.scrollTo(0,0)" allowtransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://hipaa.jotform.com/210055091090039" frameborder="0" scrolling="no" > </iframe>
                 </div>
                 <div className="bar" />
             </div>
